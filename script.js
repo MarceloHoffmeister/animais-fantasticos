f6a464eacdced1c70e1e093456428ecd16880327
Marcelo Hoffmeister <marcelohenriquehoffmeister@gmail.com>
2021-07-28 23:14:49 -0400

    navegação por tabs

---
+++ script.js
@@ -0,0 +1,19 @@
+const tabMenu = document.querySelectorAll('.js-tabmenu li')
+const tabContent = document.querySelectorAll('.js-tabcontent section')
+tabContent[0].classList.add('active')
+
+if (tabMenu.length && tabContent.length) {
+    function activeTab(index) {
+        tabContent.forEach(section => {
+            section.classList.remove('active')
+        })
+
+        tabContent[index].classList.add('active')
+    }
+
+    tabMenu.forEach((itemMenu, index) => {
+        itemMenu.addEventListener('click', () => {
+            activeTab(index)
+        })
+    })
+}
